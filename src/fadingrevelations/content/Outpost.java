22f1e7d8c6d4d7a321e8db808d6ebf8e633bf9d7
UnQuis <volokitinaroslav86@gmail.com>
2026-05-17 19:05:20 +0800

    patches

--- src/fadingrevelations/content/Outpost.java
+++ src/fadingrevelations/content/Outpost.java
@@ -1,5 +1,6 @@
 package fadingrevelations.content;
 
+import mindustry.Vars;
 import mindustry.gen.Building;
 import mindustry.type.Item;
 import mindustry.world.Block;
@@ -11,23 +12,28 @@ public class Outpost extends Block {
         solid = true;
         hasItems = true;
         itemCapacity = 5;
+        buildType = OutpostBuild::new;
     }
 
     public class OutpostBuild extends Building {
+        @Override
+        public boolean acceptItem(Building source, Item item) {
+            return items.get(item) < getMaximumAccepted(item);
+        }
+
         @Override
         public void updateTile() {
             if (items.total() > 0) {
                 Building core = core();
                 if (core != null) {
-                    items.each((item, amount) -> {
+                    for (int i = 0; i < Vars.content.items().size; i++) {
+                        Item item = Vars.content.item(i);
+                        int amount = items.get(item);
                         if (amount > 0) {
-                            int accepted = core.acceptStack(item, amount, this);
-                            if (accepted > 0) {
-                                core.handleStack(item, accepted, this);
-                                items.remove(item, accepted);
-                            }
+                            items.remove(item, amount);
+                            core.items.add(item, amount);
                         }
-                    });
+                    }
                 }
             }
         }
