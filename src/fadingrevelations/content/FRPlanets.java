e91157d90cb9867c787fa664cf1bdd8275891bdb
UnQuis <volokitinaroslav86@gmail.com>
2026-05-19 13:43:28 +0800

    Simplify FRPlanets.java - remove unsupported fields for v157.4

--- src/fadingrevelations/content/FRPlanets.java
+++ src/fadingrevelations/content/FRPlanets.java
@@ -1,25 +1,34 @@
 package fadingrevelations.content;
+
 import arc.graphics.Color;
-import mindustry.content.*;
-import mindustry.type.*;
+import mindustry.content.Blocks;
+import mindustry.content.Planets;
+import mindustry.type.Planet;
 import mindustry.graphics.g3d.NoiseMesh;
+import mindustry.graphics.g3d.HexSkyMesh;
 import mindustry.maps.generators.PlanetGenerator;
 
 public class FRPlanets {
     public static Planet cerbero, cangirus, hathor;
 
     public static void load() {
-        cerbero = new Planet("cerbero", Planets.sun, 1.5f, 4) {{
+        // CERBERO - main planet
+        cerbero = new Planet("cerbero", Planets.sun, 1.5f, 1) {{
             localizedName = "Cerbero";
             description = "A barren planet where tragedy struck long ago. Nothing is left here besides destruction and death.";
             alwaysUnlocked = true;
+            
+            // Mesh
             mesh = new NoiseMesh(this, 69, 6, Color.valueOf("2b2930"), 1.2f, 3, 0.8f, 1f, 1.5f);
-            startSector = 0;
+            
+            // Generator - simple seed
             generator = new PlanetGenerator() {{
-                baseSeed = 0;
-                seed = 0;
+                seed = 69;
             }};
+            
+            startSector = 0;
             orbitSpacing = 4;
+            radius = 1.5f;
             minZoom = 1.8f;
             allowLaunchSchematics = false;
             allowLaunchLoadout = false;
@@ -38,20 +47,25 @@ public class FRPlanets {
             clearSectorOnLose = true;
             allowWaves = true;
             defaultCore = Blocks.coreShard;
+            parent = Planets.sun;
+            solarSystem = Planets.sun;
             allowLaunchToNumbered = false;
         }};
 
-        hathor = new Planet("hathor", cerbero, 0.3f, 4) {{
+        // HATHOR - moon of Cerbero  
+        hathor = new Planet("hathor", cerbero, 0.3f, 1) {{
             localizedName = "Hathor";
             description = "A small piece of Cerbero created by the impact of a meteor. Nothing lasts here except an ancient Cerberian base built here before everything collapsed...";
             alwaysUnlocked = true;
-            mesh = new NoiseMesh(this, 0, 4, Color.valueOf("29302b"), 0.1f, 3, 0.8f, 1f, 1.5f);
+            
+            mesh = new NoiseMesh(this, 69, 4, Color.valueOf("29302b"), 0.1f, 3, 0.8f, 1f, 1.5f);
+            
             generator = new PlanetGenerator() {{
-                baseSeed = 69420;
                 seed = 69420;
             }};
-            radius = 0.3f;
+            
             orbitSpacing = 12;
+            radius = 0.3f;
             minZoom = 1.8f;
             drawOrbit = true;
             atmosphereRadIn = 0;
@@ -67,22 +81,32 @@ public class FRPlanets {
             clearSectorOnLose = true;
             allowWaves = true;
             defaultCore = Blocks.coreShard;
+            parent = cerbero;
+            solarSystem = Planets.sun;
         }};
 
-        cangirus = new Planet("cangirus", Planets.sun, 1.2f, 4) {{
+        // CANGIRUS - second planet from sun
+        cangirus = new Planet("cangirus", Planets.sun, 1.2f, 1) {{
             localizedName = "Cangirus";
             description = "A lush planet that has land and water.";
             alwaysUnlocked = true;
-            mesh = new NoiseMesh(this, 0, 5, Color.valueOf("4b64a9"), 1.2f, 4, 1f, 0.75f, 1f);
+            
+            // Mesh
+            mesh = new NoiseMesh(this, 69, 5, Color.valueOf("4b64a9"), 1.2f, 4, 1f, 0.75f, 1f);
+            
+            // Cloud mesh for Cangirus
+            cloudMesh = new HexSkyMesh(this, 15, 0.32f, 0.08f, 6, Color.valueOf("eafffd7e"), 3, 0.7f, 1f, 0.6f);
+            
             generator = new PlanetGenerator() {{
-                baseSeed = 0;
-                seed = 0;
+                seed = 69;
             }};
+            
             allowLaunchSchematics = false;
             allowLaunchLoadout = false;
             allowSectorInvasion = true;
             startSector = 0;
             orbitSpacing = 4;
+            radius = 1.2f;
             minZoom = 1.5f;
             drawOrbit = true;
             atmosphereRadIn = 0;
@@ -100,6 +124,8 @@ public class FRPlanets {
             allowWaves = true;
             prebuildBase = false;
             defaultCore = Blocks.coreShard;
+            parent = Planets.sun;
+            solarSystem = Planets.sun;
             allowLaunchToNumbered = false;
         }};
     }
